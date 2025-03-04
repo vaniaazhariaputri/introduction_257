2c71a09d8500f4aa623c2f57925625870aecb2ff
vaniaazhariaputri <vaniaazharia@gmail.com>
2025-03-04 11:42:24 +0700

    introduction C++

---
+++ intro.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main () {
+    char JenisKelamin = 'L';
+    string jenis = "laki-laki";
+    float x = 10.5;
+    int y = 5;
+    bool chech = true;
+
+    cout << "selamat datang di TI UMY/n";
+    cout << "Yogyakarta" << endl;
+    cout << "Indonesia";
+}
